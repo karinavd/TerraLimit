c71cc6cb5bbf36da9ef8270a213aedcb906d2bc6
karinavd <karina2006.ka@gmail.com>
2026-05-21 23:07:26 +0300

    feat: add routing to a frontend

--- TerraLimit/src/App.tsx
+++ TerraLimit/src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MapComponent from './components/Map/Map';
 function App() {
   return (
     <div className="h-screen w-screen">
-      <MapComponent />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MapComponent />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
