8664ff58e1e2faf98846260b35b69f3e431d62bc
karinavd <karina2006.ka@gmail.com>
2026-05-22 12:04:49 +0300

    feat/fix: add navbar and resolve routing issues

--- TerraLimit/src/App.tsx
+++ TerraLimit/src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MapComponent from './components/Map/Map';
-import Header from './components/Header/Header';
 import type { MarkerType } from './types/MarkerType';
 import { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import LayoutWithHeader from './LayoutWithHeader';
 function App() {
   const [searchedLocation, setSearchedLocation] = useState<MarkerType | null>(
     null
@@ -11,15 +12,24 @@ function App() {
   return (
     <div className="h-screen w-screen">
       <BrowserRouter>
-        <Header onLocationSelect={setSearchedLocation} />
-        <main className="h-[calc(100vh-80px)] w-full">
-          <Routes>
+        <Routes>
+          <Route
+            element={
+              <LayoutWithHeader
+                onLocationSelect={(location: MarkerType) =>
+                  setSearchedLocation(location)
+                }
+              />
+            }
+          >
             <Route
               path="/"
               element={<MapComponent searchedLocation={searchedLocation} />}
             />
-          </Routes>
-        </main>
+          </Route>
+
+          <Route path="/menu" element={<Navbar />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
