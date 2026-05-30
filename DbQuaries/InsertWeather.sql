2df38bfc2ce3a5500a268a85248821c9d5269986
Kate Kumpanska <kate.kumpansky@gmail.com>
2026-05-30 18:05:53 +0300

    refactor: change insert quaries

--- DbQuaries/InsertWeather.sql
+++ DbQuaries/InsertWeather.sql
@@ -1,13 +1,7 @@
 INSERT INTO "Weather_Locations" (
-    "LocationId",
-    "City",
-    "Region",
-    "Country",
-    "Latitude",
-    "Longitude",
-    "Timezone"
+    "LocationId", "City", "Region", "Country", "Latitude", "Longitude", "Timezone"
 )
-SELECT DISTINCT ON ("Id") 
+SELECT DISTINCT ON ("Location_Name", "Location_Region", "Location_Country") 
     "Id" AS "LocationId",
     "Location_Name" AS "City",
     "Location_Region" AS "Region",
@@ -16,43 +10,31 @@ SELECT DISTINCT ON ("Id")
     "Location_Lon" AS "Longitude",
     "Location_TzId" AS "Timezone"
 FROM "WeatherRecords"
-ORDER BY "Id"
+ORDER BY "Location_Name", "Location_Region", "Location_Country", "Id"
 ON CONFLICT ("LocationId") DO NOTHING;
 
 INSERT INTO "Weather_Observations" (
-    "RecordId", 
-    "LocationId", 
-    "Localtime", 
-    "LastUpdated", 
-    "Condition_Text", 
-    "Condition_Code",
-    "Current_Condition_Icon"
+    "RecordId", "LocationId", "Localtime", "LastUpdated", "Condition_Text", "Condition_Code", "Current_Condition_Icon"
 )
-SELECT DISTINCT ON ("Id", "Location_Localtime") 
-    "Id" AS "RecordId",
-    "Id" AS "LocationId",
-    "Location_Localtime"::TIMESTAMP AS "Localtime",
-    "Current_LastUpdated"::TIMESTAMP AS "LastUpdated",                
-    "Current_Condition_Text" AS "Condition_Text",       
-    "Current_Condition_Code" AS "Condition_Code",
-    "Current_Condition_Icon"
-FROM "WeatherRecords"
-WHERE TRIM("Current_Condition_Text") <> ''
-ORDER BY "Id", "Location_Localtime"
+SELECT DISTINCT ON (wr."Id") 
+    wr."Id" AS "RecordId",
+    loc."LocationId" AS "LocationId",
+    wr."Location_Localtime"::timestamp without time zone AS "Localtime",
+    wr."Current_LastUpdated"::timestamp without time zone AS "LastUpdated",                
+    wr."Current_Condition_Text" AS "Condition_Text",       
+    wr."Current_Condition_Code" AS "Condition_Code",
+    wr."Current_Condition_Icon"
+FROM "WeatherRecords" wr
+JOIN "Weather_Locations" loc 
+    ON wr."Location_Name" = loc."City"
+   AND (wr."Location_Region" = loc."Region" OR (wr."Location_Region" IS NULL AND loc."Region" IS NULL))
+   AND wr."Location_Country" = loc."Country"
+WHERE TRIM(wr."Current_Condition_Text") <> ''
+ORDER BY wr."Id"
 ON CONFLICT ("RecordId") DO NOTHING;
 
 INSERT INTO "Atmosphere_Metrics" (
-    "RecordId",
-    "Temperature_C",
-    "FeelsLike_C",
-    "Humidity_pct",
-    "Wind_Speed_kph",
-    "Wind_Degree",
-    "Wind_Direction",
-    "Pressure_mb",
-    "Precipitation_mm",
-    "Cloud_Cover_pct",
-    "Uv_Index"
+    "RecordId", "Temperature_C", "FeelsLike_C", "Humidity_pct", "Wind_Speed_kph", "Wind_Degree", "Wind_Direction", "Pressure_mb", "Precipitation_mm", "Cloud_Cover_pct", "Uv_Index"
 )
 SELECT 
     "Id" AS "RecordId",                               
@@ -67,20 +49,13 @@ SELECT
     "Current_Cloud" AS "Cloud_Cover_pct",                  
     "Current_Uv" AS "Uv_Index"                      
 FROM "WeatherRecords"
+WHERE "Id" IN (SELECT "RecordId" FROM "Weather_Observations")
 ON CONFLICT ("RecordId") DO NOTHING;
 
 INSERT INTO "AirQuality_Indexes" (
-    "RecordId",
-    "CO",
-    "NO2",
-    "O3",
-    "SO2",
-    "PM25",
-    "PM10",
-    "US_EPA_Index",
-    "GB_DEFRA_Index"
+    "RecordId", "CO", "NO2", "O3", "SO2", "PM25", "PM10", "US_EPA_Index", "GB_DEFRA_Index"
 )
-SELECT DISTINCT ON ("Id") 
+SELECT 
     "Id" AS "RecordId",
     "Current_AirQuality_Co" AS "CO",
     "Current_AirQuality_No2" AS "NO2",
@@ -92,5 +67,5 @@ SELECT DISTINCT ON ("Id")
     "Current_AirQuality_GbDefraIndex" AS "GB_DEFRA_Index"
 FROM "WeatherRecords"
 WHERE "Current_AirQuality_UsEpaIndex" >= 0
-ORDER BY "Id"
+  AND "Id" IN (SELECT "RecordId" FROM "Weather_Observations")
 ON CONFLICT ("RecordId") DO NOTHING;
